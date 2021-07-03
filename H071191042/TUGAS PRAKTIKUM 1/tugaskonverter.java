e7b85f51470be16e8d7079e5adf14ba3b0396a5e
takdim <54712695+takdim@users.noreply.github.com>
2021-07-04 01:15:34 +0800

    Update tugaskonverter.java

--- H071191042/TUGAS PRAKTIKUM 1/tugaskonverter.java
+++ H071191042/TUGAS PRAKTIKUM 1/tugaskonverter.java
@@ -7,19 +7,13 @@ class tugaskonverter{
         int menit;
         int jam;
         int jam1;
-        int menit1;
-        int detik1;
+
        
         jam = (detik / 3600);
-        jam1 =  (detik % 3600);
-        menit = (jam1 / 60);
-        menit1 = (menit % 60);
-        detik = ( detik % 3600);
-        detik1 = (detik % 60);
+        menit = ((detik % 3600)/ 60);
+        detik = ( detik % 3600) % 60;
 
         System.out.printf("%02d:%02d:%02d",jam,menit,detik);
-        
-        
 
 
     }
